67f05e464e9853cd83b18b340cd60818665a52bc
Kenneth Collins <kenneth.collins@alumni.stonybrook.edu>
2021-05-15 19:11:47 -0400

    changed about to login

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './plugins/store'
 import vuetify from './plugins/vuetify'
-import router from './router'
+import router from './plugins/router'
 
 Vue.config.productionTip = false
 
