5ad3849423cf42b166c8691f8ae2200d96851353
Kenneth Collins <kenneth.collins@alumni.stonybrook.edu>
2021-05-15 19:43:14 -0400

    moved files to follow LIFT Principle

---
+++ src/store/modules/auth.js
@@ -0,0 +1,22 @@
+//store/modules/auth.js
+
+import axios from 'axios';
+const state = {
+    user: null,
+    posts: null
+};
+const getters = {
+
+};
+const actions = {
+
+};
+const mutations = {
+
+};
+export default {
+    state,
+    getters,
+    actions,
+    mutations
+};
