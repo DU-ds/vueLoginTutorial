54f2fd7a9d619ad38f95084df12230df19815128
Kenneth Collins <kenneth.collins@alumni.stonybrook.edu>
2021-05-15 20:16:45 -0400

    added css style to navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -25,3 +25,18 @@ export default {
 }
 </script>
 
+<style>
+#nav {
+  padding: 30px;
+}
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+a:hover {
+  cursor: pointer;
+}
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
