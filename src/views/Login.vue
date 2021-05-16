aef8922a9a272be5407ee0f34e1dfbdeb38ae5ff
Kenneth Collins <kenneth.collins@alumni.stonybrook.edu>
2021-05-15 20:24:09 -0400

    added login template

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="login">
-    <h1>This is the login page</h1>
+    <div>
+      <form @submit.prevent="submit">
+        <div>
+          <label for="username">Username:</label>
+          <input type="text" name="username" v-model="form.username" />
+        </div>
+        <div>
+          <label for="password">Password:</label>
+          <input type="password" name="password" v-model="form.password" />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+      <p v-if="showError" id="error">Username or Password is incorrect</p>
+    </div>
   </div>
 </template>
